c255ac2e890c96d1503dcd9bec64ce03a17d9257
Jack'lul <jacklul@users.noreply.github.com>
2018-01-18 19:22:08 +0100

    Update copyright header

--- public/index.php
+++ public/index.php
@@ -2,10 +2,10 @@
 /**
  * Inline Games - Telegram Bot (@inlinegamesbot)
  *
- * (c) 2017 Jack'lul <jacklulcat@gmail.com>
+ * (c) Jack'lul <jacklulcat@gmail.com>
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view
+ * the LICENSE file that was distributed with this source code.
  */
 
 use Bot\Bot;
