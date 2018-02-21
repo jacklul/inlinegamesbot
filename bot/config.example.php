e54cb6330052ff9c9b7304a854f7da7eb37fed56
Jack'lul <jacklulcat@gmail.com>
2018-02-21 19:30:48 +0100

    Update copyright notice

--- bot/config.example.php
+++ bot/config.example.php
@@ -1,5 +1,14 @@
 <?php
 /**
+ * Inline Games - Telegram Bot (@inlinegamesbot)
+ *
+ * (c) 2016-2018 Jack'lul <jacklulcat@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+/**
  * Custom configuration
  *
  * Each setting must be set using "$config['xxx']['xxx'] = 'value'" format
--- src/Command/Admin/CleansessionsCommand.php
+++ src/Command/Admin/CleansessionsCommand.php
@@ -2,10 +2,10 @@
 /**
  * Inline Games - Telegram Bot (@inlinegamesbot)
  *
- * (c) Jack'lul <jacklulcat@gmail.com>
+ * (c) 2016-2018 Jack'lul <jacklulcat@gmail.com>
  *
- * For the full copyright and license information, please view
- * the LICENSE file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
  */
 
 namespace Longman\TelegramBot\Commands\AdminCommands;
